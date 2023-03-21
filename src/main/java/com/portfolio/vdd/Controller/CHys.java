89236c312de296d46efa35616caae52a32c60c1b
DAVIDVERA <ddvera.10@gmail.com>
2023-03-21 15:19:48 -0300

    actualizacion url

--- src/main/java/com/portfolio/vdd/Controller/CHys.java
+++ src/main/java/com/portfolio/vdd/Controller/CHys.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendvdd.web.app")
 @RequestMapping("/skill")
 public class CHys {
 
